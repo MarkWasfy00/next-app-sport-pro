a9a837bd32a2f632140a6d255c4be0c1ab234b48
Mark Wasfy <76422492+MarkWasfy00@users.noreply.github.com>
2022-04-27 21:16:41 +0200

    added google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -10,14 +10,14 @@ import NextNProgress from 'nextjs-progressbar';
 function MyApp({ Component, pageProps }) {
   return(
     <>
-      <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
+      <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=G-F50FH9094V`} />
 
       <Script id='google-analytics' strategy="lazyOnload">
           {`
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              gtag('config', 'G-F50FH9094V', {
               page_path: window.location.pathname,
               });
           `}
